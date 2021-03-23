d818f9f06231dfa6f0655e922b2ef96fd22d5ee8
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2021-03-23 22:54:46 +0100

    fix: fix hacky way to import marked

--- src/SvelteMarkdown.svelte
+++ src/SvelteMarkdown.svelte
@@ -1,77 +1,11 @@
-<script context="module">
-  import * as mark from 'marked'
-  const marked = mark.default || mark
-  import Parser from './Parser.svelte'
-  import {
-    Heading,
-    Paragraph,
-    Text,
-    Image,
-    Link,
-    Em,
-    Strong,
-    Codespan,
-    Del,
-    Table,
-    TableHead,
-    TableBody,
-    TableRow,
-    TableCell,
-    List,
-    ListItem,
-    Hr,
-    Html,
-    Blockquote,
-    Code,
-  } from './renderers'
-  const defaultRenderers = {
-    heading: Heading,
-    paragraph: Paragraph,
-    text: Text,
-    image: Image,
-    link: Link,
-    em: Em,
-    strong: Strong,
-    codespan: Codespan,
-    del: Del,
-    table: Table,
-    tablehead: TableHead,
-    tablebody: TableBody,
-    tablerow: TableRow,
-    tablecell: TableCell,
-    list: List,
-    listitem: ListItem,
-    hr: Hr,
-    html: Html,
-    blockquote: Blockquote,
-    code: Code,
-  }
-  const defaultOptions = {
-    baseUrl: null,
-    breaks: false,
-    gfm: true,
-    headerIds: true,
-    headerPrefix: '',
-    highlight: null,
-    langPrefix: 'language-',
-    mangle: true,
-    pedantic: false,
-    renderer: null,
-    sanitize: false,
-    sanitizer: null,
-    silent: false,
-    smartLists: false,
-    smartypants: false,
-    tokenizer: null,
-    xhtml: false,
-  }
-</script>
-
 <script>
+  import Parser from './Parser.svelte'
+  import { Lexer, defaultOptions, defaultRenderers } from './markdown-parser'
+
   export let source = ''
   export let renderers = {}
   export let options = {}
-  const lexer = new marked.Lexer({ ...defaultOptions, ...options })
+  const lexer = new Lexer({ ...defaultOptions, ...options })
   const tokens = lexer.lex(source)
   const combinedRenderers = { ...defaultRenderers, ...renderers }
 </script>
