203d268ffc7d12899681980a396c2c604ac2ae6b
Sotaro Tommykawa <tommysouta@gmail.com>
2021-10-18 10:48:35 +0900

    fix: add slugger

--- src/SvelteMarkdown.svelte
+++ src/SvelteMarkdown.svelte
@@ -1,20 +1,28 @@
 <script>
   import Parser from './Parser.svelte'
-  import { Lexer, defaultOptions, defaultRenderers } from './markdown-parser'
+  import { Lexer, Slugger, defaultOptions, defaultRenderers } from './markdown-parser'
 
   export let source = ''
   export let renderers = {}
   export let options = {}
   export let isInline = false;
 
+  const slugger = new Slugger();
+
   let lexer;
   let tokens;
+  $: combinedOptions = { ...defaultOptions, ...options }
   $: {
-    lexer = new Lexer({ ...defaultOptions, ...options })
+    lexer = new Lexer(combinedOptions)
     
     tokens = isInline?lexer.inlineTokens(source):lexer.lex(source)
   }
   $: combinedRenderers = { ...defaultRenderers, ...renderers }
 </script>
 
-<Parser {tokens} renderers={combinedRenderers} />
+<Parser
+  {tokens}
+  {slugger}
+  renderers={combinedRenderers}
+  options={combinedOptions}
+/>
