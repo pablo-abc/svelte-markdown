1b2767e88e63e651582c514b6834b41a9bfa5225
Sotaro Tommykawa <tommysouta@gmail.com>
2021-10-18 10:48:55 +0900

    feat: add id

--- src/renderers/Heading.svelte
+++ src/renderers/Heading.svelte
@@ -1,20 +1,26 @@
 <script>
   export let depth
   export let raw
+  export let options
+  export let slugger
+
+  $: id = options.headerIds
+    ? options.headerPrefix + slugger.slug(raw.replace(/^#+/, ''))
+    : undefined
 </script>
 
 {#if depth === 1}
-  <h1><slot></slot></h1>
+  <h1 {id}><slot></slot></h1>
 {:else if depth === 2}
-  <h2><slot></slot></h2>
+  <h2 {id}><slot></slot></h2>
 {:else if depth === 3}
-  <h3><slot></slot></h3>
+  <h3 {id}><slot></slot></h3>
 {:else if depth === 4}
-  <h4><slot></slot></h4>
+  <h4 {id}><slot></slot></h4>
 {:else if depth === 5}
-  <h5><slot></slot></h5>
+  <h5 {id}><slot></slot></h5>
 {:else if depth === 6}
-  <h6><slot></slot></h6>
+  <h6 {id}><slot></slot></h6>
 {:else}
   {raw}
 {/if}
