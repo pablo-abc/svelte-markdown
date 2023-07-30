4c91b43c27317bd94f7a3c7f03e2427cd364e63b
Chukwuma Nwaugha <nwaughac@gmail.com>
2023-07-30 11:40:58 +0400

    completely replace rollup with vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import pkg from './package.json'
 
+const removeDist = (path) => path.replace('dist/', '')
 export default defineConfig({
   plugins: [svelte({ hot: !process.env.VITEST })],
   test: {
     globals: true,
     environment: 'jsdom',
+  },
+  build: {
+    lib: {
+      entry: new URL('src/index.js', import.meta.url).pathname,
+      formats: ['es', 'umd'],
+      name: pkg.name,
+      fileName: (format) =>
+        ({
+          es: removeDist(pkg.module),
+          umd: removeDist(pkg.main),
+        }[format]),
+    },
   },
 })
