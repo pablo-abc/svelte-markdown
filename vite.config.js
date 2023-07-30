d965d7acf048253ffd9fe9e12b1d725ee6aa2cee
Chukwuma Nwaugha <nwaughac@gmail.com>
2023-07-30 13:51:59 +0400

    add terser, analyze and filesize

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import analyze from 'rollup-plugin-analyzer'
+import { terser } from 'rollup-plugin-terser'
+import filesize from 'rollup-plugin-filesize'
 import pkg from './package.json'
 
-const removeDist = (path) => path.replace('dist/', '')
+const production = !process.argv.includes('--watch')
+const removeDist = (p) => p.replace('dist/', '')
+
 export default defineConfig({
   plugins: [svelte({ hot: !process.env.VITEST })],
   test: {
@@ -10,6 +15,15 @@ export default defineConfig({
     environment: 'jsdom',
   },
   build: {
+    rollupOptions: {
+      input: 'src/index.js',
+      plugins: [
+        production && terser(),
+        production && analyze(),
+        production && filesize(),
+      ],
+    },
+    sourcemap: true,
     lib: {
       entry: new URL('src/index.js', import.meta.url).pathname,
       formats: ['es', 'umd'],
