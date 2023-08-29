dfe2cc91728a0fb0dc895a2154aa6a9d6c65c990
Chukwuma Nwaugha <nwaughac@gmail.com>
2023-08-29 19:08:05 +0100

    Upgrade marked to v5 (#74)
    * bump marked to v5.1.2
    
    * refactor the conditional block in parser.svelte; reuse similar declarations
    
    * completely replace rollup with vite
    
    * add terser, analyze and filesize
    
    * revert changes made in parser.svelte

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,38 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import analyze from 'rollup-plugin-analyzer'
+import { terser } from 'rollup-plugin-terser'
+import filesize from 'rollup-plugin-filesize'
+import pkg from './package.json'
+
+const production = !process.argv.includes('--watch')
+const removeDist = (p) => p.replace('dist/', '')
 
 export default defineConfig({
   plugins: [svelte({ hot: !process.env.VITEST })],
   test: {
     globals: true,
     environment: 'jsdom',
+  },
+  build: {
+    rollupOptions: {
+      input: 'src/index.js',
+      plugins: [
+        production && terser(),
+        production && analyze(),
+        production && filesize(),
+      ],
+    },
+    sourcemap: true,
+    lib: {
+      entry: new URL('src/index.js', import.meta.url).pathname,
+      formats: ['es', 'umd'],
+      name: pkg.name,
+      fileName: (format) =>
+        ({
+          es: removeDist(pkg.module),
+          umd: removeDist(pkg.main),
+        }[format]),
+    },
   },
 })
